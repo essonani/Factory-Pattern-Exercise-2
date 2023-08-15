80b5c33882da5a8b1d96d6c8f3f60d7924421a7c
Essonani Tchakpalla <130189659+essonani@users.noreply.github.com>
2023-08-14 22:59:06 -0500

    Add files via upload

---
+++ Factory-Pattern-Exercise-2/FactoryPatternExercise2/MongoDataAccess.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryPatternExercise2
+{
+    public class MongoDataAccess : IDataAccess
+    {
+        public static List<Product> Products = new List<Product>()
+        {
+            new Product(){Name="xbox",Price=350},
+             new Product(){Name="guitar",Price=1000},
+            new Product(){Name="BLENDER",Price=50},
+             new Product(){Name="KITE",Price=35},
+        };
+        public List<Product> LoadData()
+        {
+            Console.WriteLine("i am reading from mongo data access");
+            return Products;
+        }
+
+        public void SaveData()
+        {
+            Console.WriteLine("i am saving data from mongo DataAccess");
+        }
+    }
+}
---
+++ Factory-Pattern-Exercise-2/FactoryPatternExercise2/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace FactoryPatternExercise2
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("what data base do you choose?");
+            Console.WriteLine("type :mongo");
+            Console.WriteLine("type :sql");
+            Console.WriteLine("type :list");
+            var userInput=Console.ReadLine();
+            IDataAccess db=DataAccessFactory.GetDataAccessType(userInput);
+            var products = db.LoadData();
+            db.SaveData();
+            foreach (var product in products)
+            {
+                Console.WriteLine(product.Name,product.Price);
+            }
+        }
+    }
+}
